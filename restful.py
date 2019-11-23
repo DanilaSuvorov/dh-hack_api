a4504c66b69379c17a0009a41d9d668c53e948b6
DanilaSuvorov <dansuvtim@gmail.com>
2019-11-24 01:09:22 +0300

    db_and_2get

--- restful.py
+++ restful.py
@@ -1,10 +1,42 @@
-from bottle import run, get, post, request, delete
+import sqlite3
+from bottle import run, get
 
-emojies = [{'emoji1': 'var1', 'emoji2': 'var2'}]
+emojies = [{'emoji': 'var1'}, {'like': 'var2'}]
 
 
-@get('/emoji/<emoji1>/<emoji2>')
-def getAll(emoji1, emoji2):
-    return {emoji1, emoji2}
+@get('/emoji/<emoji>/<like>')
+def getAll(emoji, like):
+    return {emoji, like}
+
 
 run(reloader=True, debug=True)
+
+
+conn = sqlite3.connect("memory")  # или :memory: чтобы сохранить в RAM
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE data_book(n INTEGER PRIMARY KEY, author text, rate text, nameofbook text)")
+
+
+cursor.execute("INSERT INTO data_book VALUES (0,'tolstoy','true','voyna i mir')")
+cursor.execute("INSERT INTO data_book VALUES (1,'pushkin','true','voyna i NE mir')")
+cursor.execute("INSERT INTO data_book VALUES (2,'tolstoy','true','voyna i NE mir')")
+cursor.execute("INSERT INTO data_book VALUES (3,'tolstoy','true','voyna i NE mir')")
+cursor.execute("INSERT INTO data_book VALUES (4,'tolstoy','true','voyna i NE mir')")
+
+
+def database_get():
+    sql = "SELECT * FROM data_book WHERE author=?"
+    cursor.execute(sql, [("pushkin")])
+    print(cursor.fetchall())
+
+
+def database_change():
+    sql = "UPDATE data_book SET author = 'Konan Doile' WHERE author = 'tolstoy'"
+
+    cursor.execute(sql)
+    conn.commit()
+
+
+"""def database_add():
+    cursor.execute("INSERT INTO data_book VALUES (id,'tolstoy','true','voyna i NE mir')")"""
+
