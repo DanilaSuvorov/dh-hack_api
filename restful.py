83bc2ab47c4526b700417c1f0d351f94e682b626
DanilaSuvorov <dansuvtim@gmail.com>
2019-11-24 13:47:24 +0300

    connection

--- restful.py
+++ restful.py
@@ -1,29 +1,40 @@
 import sqlite3
 from bottle import run, get
 
+conn = sqlite3.connect(":memory:")  # или :memory: чтобы сохранить в RAM
+cursor = conn.cursor()
+cursor.execute(
+    "CREATE TABLE data_book(n INTEGER PRIMARY KEY AUTOINCREMENT, author text, rate text, nameofbook text, genre text)")
+
+cursor.execute(
+    "INSERT INTO data_book (author, rate, nameofbook, genre) VALUES ('K. Doile','True','Sherlock Holmes','1')")
+cursor.execute("INSERT INTO data_book (author, rate, nameofbook, genre) VALUES ('W.Shakespeare','False','Hamlet','2')")
+cursor.execute(
+    "INSERT INTO data_book (author, rate, nameofbook, genre) VALUES ('J.K. Rowling','False','Harry Potter','3')")
+cursor.execute("INSERT INTO data_book (author, rate, nameofbook, genre) VALUES ('Stephen King','True','IT','4')")
+cursor.execute(
+    "INSERT INTO data_book (author, rate, nameofbook, genre) VALUES ('R.Bradbury','True','451 on Fahrenheit','5')")
+
 emojies = [{'emoji': 'var1'}, {'like': 'var2'}]
 
 
 @get('/emoji/<emoji>')
 def getAll(emoji):
-    return {emoji}
+    print(connect(emoji))
+
 
 @get('/like/<like>/<id>')
 def getAll(like, id):
     return {like, id}
 
 
+def connect(emoji):
+    sql = "SELECT * FROM data_book WHERE genre=?"
+    cursor.execute(sql, [emoji])
+    return cursor.fetchall()
+
+
 run(reloader=True, debug=True)
-
-conn = sqlite3.connect(":memory:")  # или :memory: чтобы сохранить в RAM
-cursor = conn.cursor()
-cursor.execute("CREATE TABLE data_book(n INTEGER PRIMARY KEY AUTOINCREMENT, author text, rate text, nameofbook text, genre text)")
-
-cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('K. Doile','True','Sherlock Holmes','Detective')")
-cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('W.Shakespeare','False','Hamlet','Tragedy')")
-cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('J.K. Rowling','False','Harry Potter','Fantastic')")
-cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('Stephen King','True','IT','Horror')")
-cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('R.Bradbury','True','451 on Fahrenheit','Sci-Fi')")
 
 
 # функция выводит всю БД
@@ -49,4 +60,5 @@ def database_change():
 
 # функция добавления еще одной книги(по сути, она не нужна, т.к. все мы книги мы сами забиваем)
 def database_add():
-    cursor.execute("INSERT INTO data_book  (author, rate, nameofbook) VALUES ('J. Austen','True','Persuasion','Novel')")
+    cursor.execute(
+        "INSERT INTO data_book  (author, rate, nameofbook, genre) VALUES ('J. Austen','True','Persuasion','Novel')")
