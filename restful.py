5a02c5926466976b63e5dd2892f837640cda5b81
DanilaSuvorov <dansuvtim@gmail.com>
2019-11-24 11:50:51 +0300

    likes_and_genre

--- restful.py
+++ restful.py
@@ -1,25 +1,29 @@
 import sqlite3
 from bottle import run, get
 
-"""emojies = [{'emoji': 'var1'}, {'like': 'var2'}]
+emojies = [{'emoji': 'var1'}, {'like': 'var2'}]
 
 
-@get('/emoji/<emoji>/<like>')
-def getAll(emoji, like):
-    return {emoji, like}
+@get('/emoji/<emoji>')
+def getAll(emoji):
+    return {emoji}
+
+@get('/like/<like>/<id>')
+def getAll(like, id):
+    return {like, id}
 
 
-run(reloader=True, debug=True)"""
+run(reloader=True, debug=True)
 
 conn = sqlite3.connect(":memory:")  # или :memory: чтобы сохранить в RAM
 cursor = conn.cursor()
-cursor.execute("CREATE TABLE data_book(n INTEGER PRIMARY KEY AUTOINCREMENT, author text, rate text, nameofbook text)")
+cursor.execute("CREATE TABLE data_book(n INTEGER PRIMARY KEY AUTOINCREMENT, author text, rate text, nameofbook text, genre text)")
 
-cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('K. Doile','True','Sherlock Holmes')")
-cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('W.Shakespeare','False','Hamlet')")
-cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('J.K. Rowling','False','Harry Potter')")
-cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('Stephen King','True','IT')")
-cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('R.Bradbury','True','451 on Fahrenheit')")
+cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('K. Doile','True','Sherlock Holmes','Detective')")
+cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('W.Shakespeare','False','Hamlet','Tragedy')")
+cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('J.K. Rowling','False','Harry Potter','Fantastic')")
+cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('Stephen King','True','IT','Horror')")
+cursor.execute("INSERT INTO data_book (author, rate, nameofbook) VALUES ('R.Bradbury','True','451 on Fahrenheit','Sci-Fi')")
 
 
 # функция выводит всю БД
@@ -45,4 +49,4 @@ def database_change():
 
 # функция добавления еще одной книги(по сути, она не нужна, т.к. все мы книги мы сами забиваем)
 def database_add():
-    cursor.execute("INSERT INTO data_book  (author, rate, nameofbook) VALUES ('J. Austen','True','Persuasion')")
+    cursor.execute("INSERT INTO data_book  (author, rate, nameofbook) VALUES ('J. Austen','True','Persuasion','Novel')")
